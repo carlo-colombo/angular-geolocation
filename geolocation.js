92c7ce75d43a1b852f58cf6d7ec7d417e6c9a1df
Carlo Colombo <carlo.colombo@openmindonline.it>
2013-04-29 17:34:13 +0200

    angular dep, fixes

--- geolocation.js
+++ geolocation.js
@@ -1,16 +1,21 @@
 angular.module('ngGeolocation',[])
+  .constant('options',{})
   .factory('geolocation',
-        ["$q","$rootScope"
-function ($q , $rootScope) {
+        ["$q","$rootScope","options",
+function ($q , $rootScope , options){
   return {
     position: function () {
-      var deferred = $.defer()
+      var deferred = $q.defer()
       navigator.geolocation.getCurrentPosition(function (pos) {
         $rootScope.$apply(function () {
           deferred.resolve(pos)
         })
-      })
+      }, function (error) {
+        $rootScope.$apply(function () {
+          deferred.reject(error)
+        })
+      },options)
       return deferred.promise
     }
   }
-})
+}])
